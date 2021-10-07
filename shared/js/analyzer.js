b7c1f3ebb537fcc454d02e1d31d6b18eaffee06e
beefoo <brian@youaremyjoy.org>
2021-10-07 11:04:35 -0400

    calculate spectral flux

--- shared/js/analyzer.js
+++ shared/js/analyzer.js
@@ -1,7 +1,9 @@
 var Analyzer = (function() {
 
   function Analyzer(config) {
-    var defaults = {};
+    var defaults = {
+      bufferSize: 2048
+    };
     this.opt = _.extend({}, defaults, config);
 
     this.init();
@@ -12,22 +14,37 @@ var Analyzer = (function() {
   };
 
   Analyzer.prototype.analyze = function(audioBuffer){
-    var monoChannel = audioBuffer.getChannelData(0)
-    var bufferSize = 2048
-    Meyda.bufferSize = bufferSize
+    var monoChannel = audioBuffer.getChannelData(0);
+    bufferSize = this.opt.bufferSize;
+    Meyda.bufferSize = bufferSize;
 
     var numChunks = Math.floor(monoChannel.length / bufferSize)
     var lengthPerChunk = monoChannel.length / audioBuffer.sampleRate / numChunks * 1000;  //in milliseconds
-    console.log(lengthPerChunk + ' ms per chunk.');
+    console.log(numChunks + ' chunks at ' + lengthPerChunk + ' ms per chunk.');
 
     var dataChunks = []
     console.log('Analyzing audio chunks...');
     for(var i = 0; i < numChunks; i++) {
-        var chunk = monoChannel.slice(i*bufferSize, (i+1)*bufferSize)
-        var result = Meyda.extract('amplitudeSpectrum', chunk)
-        dataChunks.push(result)
+      var chunk = monoChannel.slice(i*bufferSize, (i+1)*bufferSize)
+      var result = Meyda.extract('amplitudeSpectrum', chunk)
+      dataChunks.push(result);
     }
-    console.log(dataChunks[0]);
+
+    var spectralFlux = [0];
+    var maxSf = 0;
+    for(var i = 1; i < numChunks; i++) {
+      var sf = 0;
+      var signal = dataChunks[i];
+      var previousSignal = dataChunks[i-1];
+      for (var j = 0; j < signal.length; j++) {
+          var x = Math.abs(signal[j]) - Math.abs(previousSignal[j]);
+          sf += (x + Math.abs(x)) / 2;
+      }
+      spectralFlux.push(sf);
+      maxSf = Math.max(maxSf, sf);
+    }
+    console.log(maxSf);
+    console.log(spectralFlux);
   };
 
   return Analyzer;
